c06bd29e9ddc08d838cb635c7d14f8da66ab7d88
Sam O'Connell <sjoconnell@Sams-MacBook-Pro.local>
2017-04-13 17:02:25 -0500

    adding environment variable

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Direction from './Direction';
 import Stop from './Stop';
 import Search from './Search';
 import axios from 'axios'
-const nowUrl = 'https://server-proxy-lnifbozuny.now.sh'
+const nowUrl = process.env.REACT_APP_NOW_URL
 
 class App extends Component {
 
